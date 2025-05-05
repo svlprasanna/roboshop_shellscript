cc69a7225cf81c6b4c62875d98a871ec5da52be0
svlprasanna <svlprasanna92@gmail.com>
2025-05-05 23:56:29 +0530

    create instances

--- create_instances.sh
+++ create_instances.sh
@@ -14,6 +14,6 @@ do
         INSTANCE_TYPE="t2.micro"
     fi
     
-    IP_ADD=(aws ec2 run-instances     --image-id ami-0b4f379183e5706b9     --instance-type t2.micro     --security-group-ids sg-0d22010b6339646b9     --subnet-id subnet-04695b2ccf75059d7     --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]" --query "Instances[0].PrivateIpAddress"     --output text)
-    echo "$IP_ADD"
+    IP=(aws ec2 run-instances     --image-id ami-0b4f379183e5706b9     --instance-type t2.micro     --security-group-ids sg-0d22010b6339646b9     --subnet-id subnet-04695b2ccf75059d7     --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]" --query "Instances[0].PrivateIpAddress"     --output text)
+    echo "Ip address is: $IP"
 done
