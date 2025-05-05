bb02c1adfa0df17ec6e7e80f759ca53298378787
svlprasanna <svlprasanna92@gmail.com>
2025-05-05 21:47:57 +0530

    create instances

--- create_instances.sh
+++ create_instances.sh
@@ -14,5 +14,5 @@ do
     else
         INSTANCE_TYPE="t2.micro"
     fi
-    aws ec2 run-instances --image-id $AMI --instance-type $INSTANCE_TYPE --security-group-ids $SG_ID --subnet-id $SUBNET_ID
+    aws ec2 run-instances --image-id $AMI --instance-type $INSTANCE_TYPE --security-group-ids $SG_ID --subnet-id $SUBNET_ID --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]"
 done
