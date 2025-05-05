209b87376e3b59556235a44e4a6e40650a35344d
svlprasanna <svlprasanna92@gmail.com>
2025-05-05 21:13:21 +0530

    create instances

--- create_instances.sh
+++ create_instances.sh
@@ -3,6 +3,7 @@
 AMI=ami-0b4f379183e5706b
 SG_ID=sg-0d22010b6339646b9
 INSTANCES=("mongodb" "redis" "mysql" "rabbitmq" "catalogue "user" "cart" "shipping" "payment" "dispatch" "web")
+
 for i in "${INSTANCES[@]}"
 do  
     echo "instance creating is $i"
@@ -12,5 +13,6 @@ do
     else
         INSTANCE_TYPE="t2.micro"
     fi
-aws ec2 run-instances --image-id ami-0b4f379183e5706b9 --instance-type $INSTANCE_TYPE --security-group-ids sg-0d22010b6339646b9
+
+    aws ec2 run-instances --image-id ami-0b4f379183e5706b9 --instance-type $INSTANCE_TYPE --security-group-ids sg-0d22010b6339646b9
 done
