748fd0af3374d6ed87b11ee4cde750f97690b9b0
svlprasanna <svlprasanna92@gmail.com>
2025-05-05 21:01:12 +0530

    create instances

---
+++ create_instances.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+AMI=ami-0b4f379183e5706b
+SG_ID=sg-0d22010b6339646b9
+INSTANCES=("mongodb" "redis" "mysql" "rabbitmq" "catalogue "user" "cart" "shipping" "payment" "dispatch" "web")
+for i in "${INSTANCES[@]}"
+do
+    if [ $i == "mongodb" ] || [ $i == "mysql" ] || [ $i == "shipping" ]
+    then
+    INSTANCE_TYPE="t3.small"
+    else
+    INSTANCE_TYPE="t2.micro"
+    fi
+done
+aws ec2 run-instances --image-id ami-0b4f379183e5706b9 --instance-type $INSTANCE_TYPE --security-group-ids sg-0d22010b6339646b9
